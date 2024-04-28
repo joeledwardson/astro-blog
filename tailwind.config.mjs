1a5dcdb83544afaa4cde435a54baba1da8699b1c
joeledwardson <joel.edwardson1@gmail.com>
2024-04-28 14:19:59 +0100

    replace dark mode button styles with tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import plugin from "tailwindcss";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
